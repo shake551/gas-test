7a7dd5c0cce7a0625e13795809cc193bbe670cb4
shake551 <73379887+shake551@users.noreply.github.com>
2022-05-17 21:58:06 +0900

    [add]最終更新者、最終更新日の取得

---
+++ main.js
@@ -0,0 +1,8 @@
+function main() {
+  const url = "https://docs.google.com/spreadsheets/d/10dILgcMHG6JtWACr-_Prng72OnOGYsf_-oGERfuBJXw/edit#gid=0";
+  const id  = url.split('/d/')[1].split('/')[0];
+  console.log(id);
+  const sheet = Drive.Files.get(id);
+  console.log(sheet.lastModifyingUserName);
+  console.log(sheet.modifiedDate);
+}
